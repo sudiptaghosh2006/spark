b9657129532a7b291dc0051e19db4216feb7cc2f
sudipta ghosh <sudiptaghosh2006@gmail.com>
2023-06-19 17:03:40 +0530

    query added

--- spark-data-loader/src/main/java/com/sudipta/SalesRecordDBReader.java
+++ spark-data-loader/src/main/java/com/sudipta/SalesRecordDBReader.java
@@ -7,6 +7,7 @@ import org.apache.commons.lang3.time.StopWatch;
 import org.apache.spark.Partition;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.DataFrameReader;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SaveMode;
@@ -23,6 +24,7 @@ public class SalesRecordDBReader
     private static Logger logger = LoggerFactory.getLogger(SalesRecordDBReader.class);
 
     private static String sourceTable = "dbo.SPARK_SALES_RECORD";
+    private static String sourceTableQuery  = "( select * from dbo.SPARK_SALES_RECORD where [Country]='Finland' ) as T";
     private static String user = "user";
     private static String password = "password";
 
@@ -63,8 +65,21 @@ public class SalesRecordDBReader
 
 		logger.debug("data will be retrieved from db {} ",dbProps); //
 		Dataset<Row> dataset = sparkSession.read()
-//			.option("numPartitions",5)
-			.jdbc(jdbcUrl, sourceTable, dbProps);
+			.option("numPartitions",5)
+			.option("numPartitions", 8)
+			.option("partitionColumn", "Order ID")
+			.option("lowerBound", 1).option("upperBound", 100)
+//			.jdbc(jdbcUrl, sourceTable, dbProps);
+			.jdbc(jdbcUrl, sourceTableQuery, dbProps);
+		
+
+			
+//		Dataset<Row> dataset =sparkSession.read().format("jdbc")
+//			  .option("url", jdbcUrl)
+//			  .option("dbtable", sourceTable)
+//			  .option("user", user)
+//			  .option("password", password)
+//			  .load();
 
 		List<Partition> partitions = dataset.toJavaRDD().partitions();
 		logger.debug("RDD partition Count ::::   {} ", partitions.size());
@@ -76,51 +91,13 @@ public class SalesRecordDBReader
 
 		long rowCount = dataset.count();
 		logger.debug(" Retrieved data  count :::: {} ", rowCount);
-		dataset.show();
+		dataset.show((int)dataset.count(),false );
 		
-		dataset.write().csv("C:\\Users\\SGHOSH43\\Desktop\\SparkData\\OUTPUT\\csv\\500000SalesRecords.csv");
+//		dataset.write().csv("C:\\Users\\SGHOSH43\\Desktop\\SparkData\\OUTPUT\\csv\\500000SalesRecords.csv");
 		
 		watch.stop();
 		logger.debug(" time taken  ==> {} (ms) ", watch.getTime());
 		
-//		dataset.
-//		Dataset<Row> finalDataset = dataset.withColumn("INSERTED_ON", functions.current_timestamp());
-
-//		finalDataset.groupBy("Country").df().show();
-
-//		Dataset<Row> count = finalDataset.groupBy("Region", "Item Type").count();
-//		count.orderBy("Region").show((int) count.count(), false);
-
-//		logger.debug("row count {}", count.count());
-//		int count = (int)finalDataset.count();
-//		finalDataset.show(count,false);
-
-		/*
-		 * StopWatch watch = new StopWatch(); watch.start();
-		 * 
-		 * logger.debug("data will be saved in db  :::: {} ", rowCount); //
-		 * finalDataset.write().mode(SaveMode.Append).jdbc(jdbcUrl, destinationTable,
-		 * dbProps); watch.stop(); long result = watch.getTime();
-		 * logger.debug("Time taken  in ms  {}", result);
-		 * logger.debug("finished data insert   :::: {} ", rowCount);
-		 */
-
-//		Thread.sleep(1000000);
-
-//		finalDataset.select(functions.year("Order Date"))
-
-//		Dataset<Row> select = finalDataset.select(functions.year(finalDataset.col("Order Date")).alias("year")
-//			, 
-//			month(elevDF.date).alias('dt_month'), 
-//			dayofmonth(elevDF.date).alias('dt_day'), 
-//			dayofyear(elevDF.date).alias('dt_dayofy'), 
-//			hour(elevDF.date).alias('dt_hour'), 
-//			minute(elevDF.date).alias('dt_min'), 
-//			weekofyear(elevDF.date).alias('dt_week_no'), 
-//			unix_timestamp(elevDF.date).alias('dt_int')
-//			);
-
-//		select.show();
 
 		sparkContext.close();
 	    }
